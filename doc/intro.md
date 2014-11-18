a7e69f64a7b5363dbcd8ea80adc057725dbf0ff6
Rob Jentzema <rob@wellnessbon.nl>
2014-11-18 20:29:05 +0100

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to difficultionary
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/difficultionary/core_test.clj
@@ -0,0 +1,7 @@
+(ns difficultionary.core-test
+  (:require [clojure.test :refer :all]
+            [difficultionary.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
